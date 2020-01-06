75c32e8ed3f232688f603050cfff5275ea3383db
szTheory <szTheory@users.noreply.github.com>
2020-01-06 00:09:50 -0500

    build data structure

--- lib/markdown_by_section.ex
+++ lib/markdown_by_section.ex
@@ -42,21 +42,62 @@ defmodule MarkdownBySection do
       html
       |> hash_from_list_item_html
     end)
+    |> List.flatten()
   end
 
   defp hash_from_list_item_html(html) do
-    list_item_html_links(html)
-    |> Enum.map(fn x ->
-      {_, attrs, name} = x
+    list =
+      list_item_html_links(html)
+      |> Enum.map(fn x ->
+        # IO.inspect(x)
+        {_, attrs, [name]} = x
 
-      href =
-        Enum.find_value(attrs, fn x ->
-          {"href", url} = x
-          url
-        end)
+        href =
+          Enum.find_value(attrs, fn x ->
+            {"href", url} = x
+            url
+          end)
 
-      %{name: name, url: href}
-    end)
+        %{name: name, url: href}
+      end)
+
+    category_name =
+      list
+      |> List.first()
+      |> Map.get(:name)
+
+    transformed_list =
+      list
+      |> Enum.map(fn x ->
+        expand_list_item(category_name, x)
+      end)
+      |> Enum.map(fn x ->
+        Map.delete(x, :name)
+      end)
+
+    %{name: category_name, entries: transformed_list}
+  end
+
+  defp expand_list_item(category_name, list_item) do
+    # IO.inspect(list_item)
+    name = Map.get(list_item, :name)
+    type = if Regex.match?(~r/\[erl/, name), do: :erlang, else: :elixir
+
+    is_num = Regex.match?(~r/\[(\d)\]/, name)
+
+    num =
+      if is_num do
+        name
+        |> String.replace(~r/\[(\d)\]/, "\\g{1}")
+        |> String.trim()
+      else
+        "1"
+      end
+
+    list_item
+    |> Map.put(:name, category_name)
+    |> Map.put(:type, type)
+    |> Map.put(:num, String.to_integer(num))
   end
 
   defp list_item_html_links(html) do
